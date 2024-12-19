e662b900debe3703ed842ad67b4db9d2e0b2a37f
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2024-12-19 16:04:17 -0300

    add User model and update details endpoint to return user object

--- src/main/java/com/course/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/course/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -7,17 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.course.springboot.webapp.springboot_web.models.User;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
   @GetMapping("/details")
   public Map<String, Object> details() {
+
+    User user = new User("Antonio", "Espinoza");
+
     Map<String, Object> body = new HashMap<>();
 
     body.put("title", "Hola Mundo Spring Boot");
-    body.put("name", "Antonio");
-    body.put("lastname", "Espinoza");
+    body.put("user", user);
+
     return body;
   }
 
