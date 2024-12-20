8229c07a7aba39240f45c2ae6ec2d3dbc2a41f88
AntonioER9 <aespinozareyes9@gmail.com>
2024-12-20 19:44:06 -0300

    add create endpoint to PathVariableController to handle user creation

--- src/main/java/com/course/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/course/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,10 +3,13 @@ package com.course.springboot.webapp.springboot_web.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.course.springboot.webapp.springboot_web.models.User;
 import com.course.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/var")
@@ -17,6 +20,12 @@ public class PathVariableController {
     ParamDto param = new ParamDto();
     param.setMessage(message);
     return param;
+  }
+
+  @PostMapping("/create")
+  public User creatUser(@RequestBody User user) {
+    // Do something with the user
+    return user;
   }
 
 }
